cbbc39917eaa7d9ca22e01378fe88c96a9d4a91c
mcmhav <mcmhav@gmail.com>
2019-02-23 13:17:22 +0100

    add scrollbarshingy

--- js/src/features/AmGraph/AmGraph.js
+++ js/src/features/AmGraph/AmGraph.js
@@ -4,13 +4,17 @@ import * as am4charts from '@amcharts/amcharts4/charts';
 
 class Graph extends Component {
   createSeries = (chart, column) => {
-    const seriea = chart.series.push(new am4charts.LineSeries());
-    seriea.dataFields.valueY = column;
-    seriea.dataFields.categoryX = 'epoch';
-    seriea.name = column;
-    seriea.strokeWidth = 1;
-    seriea.tensionX = 0.7;
+    const series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = column;
+    series.dataFields.categoryX = 'epoch';
+    series.name = column;
+    series.strokeWidth = 1;
+    series.tensionX = 0.7;
     // seriea.bullets.push(new am4charts.CircleBullet());
+
+    // const scrollbarX = new am4charts.XYChartScrollbar();
+    // scrollbarX.series.push(series);
+    // chart.scrollbarX = scrollbarX;
   };
   componentDidMount() {
     // https://codepen.io/team/amcharts/pen/bMgEbx?editors=0010
@@ -26,10 +30,20 @@ class Graph extends Component {
 
     // Create value axis
     const valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    var dateAxis2 = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis2.renderer.grid.template.location = 0;
+    dateAxis2.renderer.labels.template.fill = am4core.color('#dfcc64');
 
     // Create series
     this.createSeries(chart, 'temp');
     // this.createSeries('temp');
+
+    chart.cursor = new am4charts.XYCursor();
+    // chart.cursor.xAxis = dateAxis2;
+
+    const scrollbarX = new am4charts.XYChartScrollbar();
+    // scrollbarX.series.push(series);
+    chart.scrollbarX = scrollbarX;
 
     // Add legend
     chart.legend = new am4charts.Legend();
